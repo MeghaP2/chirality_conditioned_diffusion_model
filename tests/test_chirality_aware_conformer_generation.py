b505b50ad4dcca22af3e0cca832e802d715c1bda
Megha Parashar <parasharmegha02@gmail.com>
2026-08-21 11:19:20 +0530

    Delete tests/test_chirality_aware_conformer_generation.py

--- tests/test_chirality_aware_conformer_generation.py
+++
@@ -1,8 +0,0 @@
-"""Tests for `chirality_aware_conformer_generation` package."""
-
-import chirality_aware_conformer_generation
-
-
-def test_import():
-    """Verify the package can be imported."""
-    assert chirality_aware_conformer_generation
